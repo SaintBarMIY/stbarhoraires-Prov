6be33bf0c0ec9c2ad170472b0dbcb0ed2d3e3661
MIY <miy@belgacom.net>
2025-07-24 09:52:34 +0200

    Re-verify Tailwind config and trigger new Netlify build

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 @tailwind components;
 @tailwind utilities;
 
-/* Vous pouvez ajouter votre propre CSS personnalisé ici si nécessaire */
-/* Par exemple, pour définir la police Inter globalement si elle n'est pas déjà chargée */
+/* Import de la police Inter pour une meilleure esthétique */
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
 body {
